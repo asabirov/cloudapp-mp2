b88a797d3c9315f16ff4c43c6982732b573b3ed8
a.sabirov <mail@artursabirov.ru>
2015-09-08 13:07:41 +0400

    implemented OrphanPages

--- OrphanPages.java
+++ OrphanPages.java
@@ -26,20 +26,50 @@ public class OrphanPages extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
-        //TODO
+        Job job = Job.getInstance(this.getConf(), "Orphan Pages");
+
+        job.setMapperClass(LinkCountMap.class);
+        job.setMapOutputKeyClass(Text.class); // id of page to which link exists
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setReducerClass(OrphanPageReduce.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(NullWritable.class); // only page id needed
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setJarByClass(OrphanPages.class);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 
     public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            //TODO
+            String pageId = value.toString().replaceFirst(":\\s.*", "");
+            String[] linkedPageIds = value.toString().replaceFirst("[0-9]+:\\s", "").split("\\s");
+
+            IntWritable pageIdInt = new IntWritable(Integer.parseInt(pageId));
+            context.write(pageIdInt, new IntWritable(1));
+
+            for (String id:  linkedPageIds) {
+                IntWritable idInt = new IntWritable(Integer.parseInt(id));
+                context.write(idInt, new IntWritable(1));
+            }
         }
     }
 
     public static class OrphanPageReduce extends Reducer<IntWritable, IntWritable, IntWritable, NullWritable> {
         @Override
         public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            //TODO
+            Integer linksCount = 0;
+            for (IntWritable value: values) {
+                linksCount += value.get();
+            }
+
+            if (linksCount == 0) {
+                context.write(key, NullWritable.get());
+            }
         }
     }
 }
