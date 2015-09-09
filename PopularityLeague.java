457a4684dba40b783ea773309c4e1a31904dfde3
a.sabirov <mail@artursabirov.ru>
2015-09-09 10:17:31 +0400

    implemented PopularityLeague

--- PopularityLeague.java
+++ PopularityLeague.java
@@ -100,18 +100,41 @@ public class PopularityLeague extends Configured implements Tool {
         }
     }
 
+    public static String readHDFSFile(String path, Configuration conf) throws IOException{
+        Path pt=new Path(path);
+        FileSystem fs = FileSystem.get(pt.toUri(), conf);
+        FSDataInputStream file = fs.open(pt);
+        BufferedReader buffIn=new BufferedReader(new InputStreamReader(file));
+
+        StringBuilder everything = new StringBuilder();
+        String line;
+        while( (line = buffIn.readLine()) != null) {
+            everything.append(line);
+            everything.append("\n");
+        }
+        return everything.toString();
+    }
+
     public static class LinksCounterMapper extends Mapper<Object, Text, IntWritable, IntWritable> {
+        List<String> league;
+
+        @Override
+        protected void setup(Context context) throws IOException,InterruptedException {
+            Configuration conf = context.getConfiguration();
+            String league_file = conf.get("league");
+            this.league = Arrays.asList(readHDFSFile(league_file, conf).split("\n"));
+        }
+
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String pageId = value.toString().replaceFirst(":\\s.*", "");
             String[] linkedPageIds = value.toString().replaceFirst("[0-9]+:\\s", "").split("\\s");
 
-            IntWritable pageIdInt = new IntWritable(Integer.parseInt(pageId.trim()));
-            context.write(pageIdInt, new IntWritable(0));
-
             for (String id:  linkedPageIds) {
-                IntWritable idInt = new IntWritable(Integer.parseInt(id.trim()));
-                context.write(idInt, new IntWritable(1));
+                if (league.contains(id)) {
+                    System.out.println("[LinksCounterMapper] Sent to reducer: " + id);
+                    IntWritable idInt = new IntWritable(Integer.parseInt(id.trim()));
+                    context.write(idInt, new IntWritable(1));
+                }
             }
         }
     }
@@ -128,16 +151,95 @@ public class PopularityLeague extends Configured implements Tool {
         }
     }
 
-    public static class PageRankerMapper extends Mapper<Object, Text, IntWritable, IntWritable> {
+    public static class PageRankerMapper extends Mapper<Text, Text, NullWritable, IntArrayWritable> {
         @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+            Integer[] values = {toInteger(key), toInteger(value)};
+            IntArrayWritable val = new IntArrayWritable(values);
+            context.write(NullWritable.get(), val);
+        }
 
+        private Integer toInteger(Text value) {
+            return Integer.parseInt((value.toString()));
         }
     }
 
-    public static class PageRankerReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+    public static class PageRankerReducer extends Reducer<NullWritable, IntArrayWritable, IntWritable, IntWritable> {
         @Override
-        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        public void reduce(NullWritable key, Iterable<IntArrayWritable> items, Context context) throws IOException, InterruptedException {
+            for (IntArrayWritable item : items) {
+                Integer[] currentPage = (Integer[]) item.toArray();
+
+                Integer pr = calculatePageRank(currentPage, items);
+                System.out.println(currentPage[0] + " - " + pr);
+
+                context.write(new IntWritable(currentPage[0]), new IntWritable(pr));
+            }
         }
+
+        private Integer calculatePageRank(Integer[] currentPage, Iterable<IntArrayWritable> pages) {
+            Integer pr = 0;
+            for (IntArrayWritable page : pages) {
+                Integer[] pageData = (Integer[]) page.toArray();
+                if (!currentPage[0].equals(pageData[0]) && currentPage[1] > pageData[1]) {
+                    pr += 1;
+                }
+            }
+
+            return pr;
+        }
+    }
+}
+
+class Pair<A extends Comparable<? super A>,
+        B extends Comparable<? super B>>
+        implements Comparable<Pair<A, B>> {
+
+    public final A first;
+    public final B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <A extends Comparable<? super A>,
+            B extends Comparable<? super B>>
+    Pair<A, B> of(A first, B second) {
+        return new Pair<A, B>(first, second);
+    }
+
+    @Override
+    public int compareTo(Pair<A, B> o) {
+        int cmp = o == null ? 1 : (this.first).compareTo(o.first);
+        return cmp == 0 ? (this.second).compareTo(o.second) : cmp;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * hashcode(first) + hashcode(second);
+    }
+
+    private static int hashcode(Object o) {
+        return o == null ? 0 : o.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Pair))
+            return false;
+        if (this == obj)
+            return true;
+        return equal(first, ((Pair<?, ?>) obj).first)
+                && equal(second, ((Pair<?, ?>) obj).second);
+    }
+
+    private boolean equal(Object o1, Object o2) {
+        return o1 == o2 || (o1 != null && o1.equals(o2));
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ')';
     }
 }
